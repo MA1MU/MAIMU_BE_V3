3171cc7af32259af8c97be2eeb578ed28af3d270
kimh7537 <kimh7537@gmail.com>
2025-01-07 18:19:31 +0900

    Refactor: Member 레포지토리 수정

---
+++ src/main/java/com/example/chosim/chosim/domain/auth/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.chosim.chosim.domain.auth.repository;
+
+import com.example.chosim.chosim.domain.auth.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUniqueId(String uniqueId);
+
+    Optional<Member> findByNickName(String nickName);
+}
