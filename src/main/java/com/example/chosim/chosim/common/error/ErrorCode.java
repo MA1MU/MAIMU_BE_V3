670a07c7d4afd8adc61673c6d53d28decdcb82fc
kimh7537 <kimh7537@gmail.com>
2025-01-07 18:10:35 +0900

    Refactor: 에러 코드 enum 타입 수정

--- src/main/java/com/example/chosim/chosim/common/error/ErrorCode.java
+++ src/main/java/com/example/chosim/chosim/common/error/ErrorCode.java
@@ -7,12 +7,30 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
-    NICKNAME_DUPLICATE(HttpStatus.CONFLICT, "해당 닉네임을 가진 사용자가 존재합니다."),
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"해당 회원이 존재하지 않습니다."),
-    JWT_NOT_EXISTS(HttpStatus.UNAUTHORIZED, "Jwt 토큰이 존재하지 않습니다."),
-    LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "로그인 요청에 실패했습니다.");
+    // DTO 에서 발생하는 에러
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "I-001", "입력 값이 잘못되었습니다."),
+    // 404 오류 -> 객체를 찾을 수 없는 문제
+    ENTITY_NOT_FOUND(HttpStatus.NOT_FOUND, "I-201", "해당 Entity를 찾을 수 없습니다."),
 
+    //멤버 관련 오류
+    NICKNAME_DUPLICATE(HttpStatus.CONFLICT, "M-001", "해당 닉네임을 가진 사용자가 존재합니다."),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"M-002", "해당 회원이 존재하지 않습니다."),
+
+    //로그인 AUTH
+    JWT_NOT_EXISTS(HttpStatus.UNAUTHORIZED, "T-001", "Jwt 토큰이 존재하지 않습니다."),
+    LOGIN_FAIL(HttpStatus.UNAUTHORIZED, "T-002", "로그인 요청에 실패했습니다."),
+
+    //Group
+    GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "G-001", "그룹을 찾을 수 없습니다."),
+
+    //Maimu
+    MAIMU_NOT_FOUND(HttpStatus.NOT_FOUND, "M-001", "마이무를 찾을 수 없습니다."),
+
+    //상위 예외 처리
+    GENERAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G-001", "예상치 못한 오류가 발생했습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "G-002", "서버 내부 오류가 발생했습니다.");
+    
     private final HttpStatus httpStatus;
+    private final String code;
     private final String message;
-
 }
