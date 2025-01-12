a06dff12679b2584cbda077419d3750bc0a3e2be
kimh7537 <kimh7537@gmail.com>
2025-01-12 14:56:45 +0900

    Feat: RefreshToken관련 코드 추가

---
+++ src/main/java/com/example/chosim/chosim/common/jwt/RefreshToken.java
@@ -0,0 +1,23 @@
+package com.example.chosim.chosim.common.jwt;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@RedisHash(value = "jwtToken", timeToLive = 60 * 60 * 24 * 3)
+public class RefreshToken {
+
+    @Id
+    private Long id;
+
+    private String refreshToken;
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
