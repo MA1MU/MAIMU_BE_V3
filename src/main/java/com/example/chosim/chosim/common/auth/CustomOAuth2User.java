589a45d48104434a327eb8d1a67da90f148cc778
kimh7537 <kimh7537@gmail.com>
2025-01-13 04:14:51 +0900

    Fix: CustomOAuth2User 최종 완성

--- src/main/java/com/example/chosim/chosim/common/auth/CustomOAuth2User.java
+++ src/main/java/com/example/chosim/chosim/common/auth/CustomOAuth2User.java
@@ -1,48 +1,41 @@
 package com.example.chosim.chosim.common.auth;
 
 import com.example.chosim.chosim.domain.auth.entity.Member;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 
-
+@Data
 public class CustomOAuth2User implements OAuth2User {
 
-    private Member member;
-    private Map<String, Object> attributes;
-    private String attributeKey;
-
-    public CustomOAuth2User(Member member, Map<String,Object> attributes, String attributeKey){
-        this.member = member;
-        this.attributes = attributes;
-        this.attributeKey = attributeKey;
-    }
+    private final Member member;
+    private final Map<String, Object> attributes;
+    private final String attributeKey;
 
     @Override
     public Map<String, Object> getAttributes() {
-        return null;
+        return attributes;
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collection = new ArrayList<>();
-
-        collection.add(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return member.getRole().getKey();
-            }
-        });
-
-        return collection;
+        return List.of(new SimpleGrantedAuthority(member.getRole().getKey()));
     }
 
     @Override
     public String getName() {
         return member.getName();
+    }
+
+    public String getProvider() {
+        return member.getProvider();
     }
 
     public String getProviderId() {
