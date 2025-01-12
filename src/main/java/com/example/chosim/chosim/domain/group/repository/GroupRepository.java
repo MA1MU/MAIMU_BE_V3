c0d13b5fd0c8c12673f55158da5576ffb261a27b
kimh7537 <kimh7537@gmail.com>
2025-01-12 14:32:33 +0900

    Refactor: GroupRepository 변경

--- src/main/java/com/example/chosim/chosim/domain/group/repository/GroupRepository.java
+++ src/main/java/com/example/chosim/chosim/domain/group/repository/GroupRepository.java
@@ -2,9 +2,12 @@ package com.example.chosim.chosim.domain.group.repository;
 
 import com.example.chosim.chosim.domain.group.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
-    List<Group> findByUserEntity_IdOrderByIdAsc(Long id);
+    @Query("SELECT g FROM Group g JOIN FETCH g.member WHERE g.member.id = :id ORDER BY g.id ASC")
+    List<Group> findByMember_IdOrderByIdAsc(@Param("id") Long id);
 }
