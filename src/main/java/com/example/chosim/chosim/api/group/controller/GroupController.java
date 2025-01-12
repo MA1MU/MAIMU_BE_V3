4f96b712ff152d82087e23910fe465624ba30040
kimh7537 <kimh7537@gmail.com>
2025-01-12 14:32:01 +0900

    Refactor: GroupController 변경

--- src/main/java/com/example/chosim/chosim/api/group/controller/GroupController.java
+++ src/main/java/com/example/chosim/chosim/api/group/controller/GroupController.java
@@ -1,14 +1,17 @@
 package com.example.chosim.chosim.api.group.controller;
 
-import com.example.chosim.chosim.api.group.dto.GroupCreate;
-import com.example.chosim.chosim.api.group.dto.GroupEdit;
-import com.example.chosim.chosim.api.group.dto.GroupListResponse;
+import com.example.chosim.chosim.api.group.dto.GroupRequest;
 import com.example.chosim.chosim.api.group.dto.GroupResponse;
-import com.example.chosim.chosim.common.jwt.JWTUtil;
+import com.example.chosim.chosim.common.dto.ResponseDTO;
 import com.example.chosim.chosim.domain.group.service.GroupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,49 +20,42 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/api/group")
 @RequiredArgsConstructor
+@Tag(name = "그룹 관련 API", description = "사용자는 자신의 사물함 페이지에서 그룹에 관한 기능에 접근할 수 있다.")
 public class GroupController {
 
     private final GroupService groupService;
-    private final JWTUtil jwtUtil;
 
-//    @PostMapping("/{username}")
-//    public void group(@PathVariable String username, @RequestBody @Valid GroupCreate groupCreate){
-//        groupService.createGroup(username, groupCreate);
-//    }
-    @PostMapping
-    public void group(
-            @RequestHeader("Authorization") String accessToken,
-            @RequestBody @Valid GroupCreate groupCreate){
-        groupService.createGroup(jwtUtil.getUsername(accessToken) , groupCreate);
-    }
-
-
-//    @GetMapping("/{username}/all")
-//    public GroupListResponse groupList(@PathVariable String username){
-//        List<GroupResponse> all = groupService.getList(username);
-//        return new GroupListResponse(all);
-//    }
     @GetMapping("/all")
-    public GroupListResponse groupList(
-            @RequestHeader("Authorization") String accessToken){
-        List<GroupResponse> all = groupService.getList(jwtUtil.getUsername(accessToken));
-        return new GroupListResponse(all);
+    @Operation(summary = "모든 그룹 정보 가져오기", description = "로그인한 후 나의 사물함 페이지에서 내가 가진 모든 그룹 정보를 가져온다.")
+    public ResponseEntity<ResponseDTO> getGroupList(@AuthenticationPrincipal Long memberId){
+        List<GroupResponse> groupResponseList = groupService.getList(memberId);
+        return ResponseEntity.status(HttpStatus.OK).body(new ResponseDTO(groupResponseList));
     }
 
-    @GetMapping(value = "/{groupId}", produces = "application/json")
-    public GroupResponse get(@PathVariable Long groupId){
-        return groupService.get(groupId);
+    @GetMapping("/{groupId}")
+    @Operation(summary = "개별 그룹 정보 가져오기", description = "로그인한 후 나의 사물함 페이지에 존재하는 개별 그룹에 대한 정보를 가져온다.")
+    public ResponseEntity<GroupResponse> getGroup(@PathVariable Long groupId){
+        return ResponseEntity.status(HttpStatus.OK).body(groupService.getGroup(groupId));
     }
 
+    @PostMapping
+    @Operation(summary = "새로운 그룹 추가하기", description = "로그인한 후 나의 사물함 페이지에 새로운 그룹을 추가한다.")
+    public ResponseEntity<GroupResponse> createGroup(@AuthenticationPrincipal Long memberId,
+                                                     @Valid @RequestBody GroupRequest groupRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(groupService.createGroup(memberId, groupRequest));
+    }
 
     @PatchMapping("/{groupId}")
-    public void edit(@PathVariable Long groupId, @RequestBody @Valid GroupEdit groupEdit){
-        groupService.edit(groupId, groupEdit);
+    @Operation(summary = "기존 그룹 수정하기", description = "나의 사물함 페이지에 있는 그룹 하나를 수정한다.")
+    public ResponseEntity<GroupResponse> editGroup(@PathVariable Long groupId, @Valid @RequestBody GroupRequest groupRequest){
+        return ResponseEntity.status(HttpStatus.OK).body(groupService.editGroup(groupId, groupRequest));
     }
 
     @DeleteMapping("/{groupId}")
-    public void delete(@PathVariable Long groupId){
-        groupService.delete(groupId);
+    @Operation(summary = "기존 그룹 삭제하기", description = "나의 사물함 페이지에 있는 그룹 하나를 삭제한다.")
+    public ResponseEntity<Void> deleteGroup(@PathVariable Long groupId){
+        groupService.deleteGroup(groupId);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
