eaa4aa7a52555d10b48874d7f80b495af2f5e5df
kimh7537 <kimh7537@gmail.com>
2025-01-19 17:21:37 +0900

    Feat: MAIMU 관련 쿼리 추가

--- src/main/java/com/example/chosim/chosim/domain/maimu/repository/MaimuRepository.java
+++ src/main/java/com/example/chosim/chosim/domain/maimu/repository/MaimuRepository.java
@@ -2,8 +2,15 @@ package com.example.chosim.chosim.domain.maimu.repository;
 
 import com.example.chosim.chosim.domain.maimu.entity.Maimu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MaimuRepository extends JpaRepository<Maimu, Long>, MaimuRepositoryCustom{
+
+    @Modifying
+    @Query("DELETE FROM Maimu m WHERE m.group.id = :groupId")
+    void deleteByGroupId(@Param("groupId") Long groupId);
 }
