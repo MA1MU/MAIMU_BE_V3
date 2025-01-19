ae64a7eb7ab4315cdbddb698fbb3fa6139008340
kimh7537 <kimh7537@gmail.com>
2025-01-19 20:16:37 +0900

    Refactor: 읽지 않은 마이무 찾는 SQL 추가

--- src/main/java/com/example/chosim/chosim/domain/maimu/repository/MaimuRepository.java
+++ src/main/java/com/example/chosim/chosim/domain/maimu/repository/MaimuRepository.java
@@ -10,6 +10,9 @@ import java.util.List;
 
 public interface MaimuRepository extends JpaRepository<Maimu, Long>, MaimuRepositoryCustom{
 
+    @Query("SELECT COUNT(m) FROM Maimu m WHERE m.group.id = :groupId AND m.isRead = false")
+    Long countByGroupIdAndIsReadFalse(@Param("groupId") Long groupId);
+
     @Modifying
     @Query("DELETE FROM Maimu m WHERE m.group.id = :groupId")
     void deleteByGroupId(@Param("groupId") Long groupId);
