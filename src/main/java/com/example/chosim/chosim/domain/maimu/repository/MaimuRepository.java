787a532d5fc935ca01f1ae7e1723723ab2c7446d
kimh7537 <kimh7537@gmail.com>
2026-02-02 23:41:49 +0900

    Feat: 이메일 관련 함수 추가

--- src/main/java/com/example/chosim/chosim/domain/maimu/repository/MaimuRepository.java
+++ src/main/java/com/example/chosim/chosim/domain/maimu/repository/MaimuRepository.java
@@ -16,4 +16,11 @@ public interface MaimuRepository extends JpaRepository<Maimu, Long>, MaimuReposi
     @Modifying
     @Query("DELETE FROM Maimu m WHERE m.group.id = :groupId")
     void deleteByGroupId(@Param("groupId") Long groupId);
+
+    @Query("SELECT m FROM Maimu m JOIN FETCH m.group g JOIN FETCH g.member WHERE m.isRead = false AND m.isNotified = false")
+    List<Maimu> findAllIsNotifiedMaimusWithMember();
+
+    @Modifying(clearAutomatically = true) // 쿼리 실행 후 영속성 컨텍스트를 비워 데이터 정합성 유지
+    @Query("UPDATE Maimu m SET m.isNotified = true WHERE m.id IN :ids")
+    void updateNotifiedStatus(@Param("ids") List<Long> ids);
 }
