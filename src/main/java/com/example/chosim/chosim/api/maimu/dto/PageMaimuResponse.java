7d82d6afc7bbd8ff8e2438c1112856a91604c3f0
kimh7537 <kimh7537@gmail.com>
2026-02-03 23:27:26 +0900

    Refactor : 로직 변경

--- src/main/java/com/example/chosim/chosim/api/maimu/dto/PageMaimuResponse.java
+++ src/main/java/com/example/chosim/chosim/api/maimu/dto/PageMaimuResponse.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 @Getter  // Lombok @Getter 추가
 public class PageMaimuResponse<T> {
+    private final String nickName;
     private final List<T> data;
     private final int currentPage;
     private final int totalPage;
 
-    public PageMaimuResponse(Page<T> page) {
+    public PageMaimuResponse(String nickName, Page<T> page) {
+        this.nickName = nickName;
         this.data = page.getContent();
         this.currentPage = page.getNumber();
         this.totalPage = page.getTotalPages();
